89dfb2ec031ce60b71f5e8f8dbaa3375130b9cf7
moyarzun <mauricio2141@gmail.com>
2024-06-24 01:35:54 -0400

    feat: migracion, seeds y CRUD para roles de usuario

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   has_many :table_users
   has_many :tables, through: :table_users
   has_many :rounds
   has_many :tournaments
+
+  belongs_to :role
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_06_185152) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_24_052656) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "roles", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "table_users", force: :cascade do |t|
     t.bigint "table_id", null: false
